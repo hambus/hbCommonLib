2be1428473dc4d52931c9af371bb311770d63f25
Darryl Wagoner <darryl.wagoner@statellite.com>
2020-07-03 20:43:54 -0500

    check point not working

--- HambusCommonLibrary/DataLib/BusConfigurationDB.cs
+++ HambusCommonLibrary/DataLib/BusConfigurationDB.cs
@@ -8,8 +8,13 @@ namespace CoreHambusCommonLibrary.DataLib
     public int? Id { get; set; }
     public string Name { get; set; } = "";
     public int Version { get; set; }
-    public BusType BusType { get; set; }
+    public BusType BusType { get; set; } = BusType.Unknown;
 
     public string Configuration { get; set; } = "{}";
+    public override string ToString()
+    {
+      var bType = BusType.ToString();
+      return $"{Name} - {bType}";
+    }
   }
 }
